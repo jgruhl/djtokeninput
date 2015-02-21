318b37fc3f818394955ead0b0bba43307379b24d
Jason Gruhl <jgruhl@gmail.com>
2015-02-21 15:30:35 -0800

    removing defaults module

--- lib/djtokeninput/urls.py
+++ lib/djtokeninput/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns("",
   url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", "djtokeninput.views.search", name="djtokeninput_search")
