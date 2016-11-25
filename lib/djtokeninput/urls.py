0f751d286de3d4d581bdf38f3d181476d71be59d
Jason Gruhl <jgruhl@gmail.com>
2016-11-25 09:34:09 -0800

    More 1.10 updates

--- lib/djtokeninput/urls.py
+++ lib/djtokeninput/urls.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from django.conf.urls import url
+from djtokeninput.views import search
 
 urlpatterns = ("",
-  url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", "djtokeninput.views.search", name="djtokeninput_search")
+  url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", search, name="djtokeninput_search")
 )
