7a03ac2d8aa47b8ac7bd00426d41443b5b8e8b30
Jason Gruhl <jgruhl@gmail.com>
2016-11-25 21:34:56 -0800

    Removing empty pattern

--- lib/djtokeninput/urls.py
+++ lib/djtokeninput/urls.py
@@ -3,6 +3,6 @@
 from django.conf.urls import url
 from djtokeninput.views import search
 
-urlpatterns = ("",
+urlpatterns = (
   url(r"^(?P<app_label>\w+)/(?P<model>\w+)$", search, name="djtokeninput_search")
 )
